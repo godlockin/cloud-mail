20e01675431ff8464e7f83fff770b8dfe2a3ca15
aslost <225811079+aslost@users.noreply.github.com>
2026-08-25 12:27:20 +0800

    release: v3.2.0

--- mail-vue/src/views/sys-setting/index.vue
+++ mail-vue/src/views/sys-setting/index.vue
@@ -871,7 +871,7 @@ defineOptions({
   name: 'sys-setting'
 })
 
-const currentVersion = 'v3.1.0'
+const currentVersion = 'v3.2.0'
 const hasUpdate = ref(false)
 let getUpdateErrorCount = 1;
 const {t, locale} = useI18n();
